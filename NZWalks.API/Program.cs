16b82731aeb9ae57a6147ff43ddf1d2d860bb20e
Kunlexi <adesinataofik5@gmail.com>
2023-09-04 01:51:00 +0100

    update

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -13,6 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -52,6 +55,9 @@ Options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksAuthConne
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+builder.Services.AddScoped<IImageRepository, LocalImageRepository>();
+
+//builder.Services.AddScoped<IImageRepository, LocalImageRepository>;
 
 builder.Services.AddAutoMapper(typeof(AutomapperProfiles));
 
